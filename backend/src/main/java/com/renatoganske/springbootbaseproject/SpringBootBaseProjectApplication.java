882ea13dbd34a9c573823b35f0490113f24a374c
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-01-11 10:04:59 -0300

    feat: Criação do projeto no Spring

---
+++ backend/src/main/java/com/renatoganske/springbootbaseproject/SpringBootBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.renatoganske.springbootbaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaseProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/renatoganske/springbootbaseproject/SpringBootBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoganske.springbootbaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
