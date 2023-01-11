b4851273c6b4a391f2a15144f5dfaeeb7426b996
Renato Ganske <renato.j@lyncas.net>
2023-01-11 16:50:36 -0300

    feat: Implementados métodos getAll e getById

--- backend/src/main/java/com/renatoganske/springbootbaseproject/service/UserService.java
+++ backend/src/main/java/com/renatoganske/springbootbaseproject/service/UserService.java
@@ -1,4 +1,24 @@
 package com.renatoganske.springbootbaseproject.service;
 
+import com.renatoganske.springbootbaseproject.domain.entities.User;
+import com.renatoganske.springbootbaseproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
 }
